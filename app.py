d18fb1d4282de42c1defdad5967fd2def6f8927a
AlexanderSatryo135 <dhitoalex123@gmail.com>
2026-01-15 17:09:50 +0700

    Update Print

--- app.py
+++ app.py
@@ -316,5 +316,6 @@ if __name__ == '__main__':
     except: ip = "127.0.0.1"
     print(f"\n--- myDrive Cloud Server ---")
     print(f"Local: http://{ip}:5000")
-    print(f"Tailscale IP: http://100.86.93.30:5000 (Gunakan ini di HP)")
+    print(f"Tailscale IP: http://100.86.93.30:5000 Windows")
+    print(f"Tailscale IP: http://100.95.26.39:5000 MacOS")
     app.run(host='0.0.0.0', port=5000, debug=True)
